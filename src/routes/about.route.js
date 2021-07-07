b3882cfc15bbf99c968013311dcb79e24461012a
Minakrv <mnkrv@yahoo.com>
2021-07-07 12:00:38 +0100

    #3 add about_route

--- src/routes/about.route.js
+++ src/routes/about.route.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router;
 
-const Axios = require("axios");
-
-router.get("/", (req,res) => {
-    
+router.get("/About", (req,res) => {
+    res.send("About Page");
 })
