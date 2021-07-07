99a3d7d3d68fb842a6fc0a380763e7d6d9c1c6d2
RomanHyde <romanhyde1@gmail.com>
2021-07-08 00:45:13 +0100

    #24 added a proxy to the client's package.json file

--- src/routes/phenomenon.route.js
+++ src/routes/phenomenon.route.js
@@ -4,10 +4,8 @@ const router = express.Router();
 
 router.get("/", (req,res) => {
     res.status(200).send("Phenomenon page");
-    // res.json([
+    // res.status(200).json([
     //     {id:1, username:"someone"},
     //     {id:2, username:"no-one"}
     // ]);
 });
-
-module.exports = router;
