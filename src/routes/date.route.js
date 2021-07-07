64cfdb285120f5f7ffe041bcfd3448abec9be792
RomanHyde <romanhyde1@gmail.com>
2021-07-07 16:57:04 +0100

    #3 fixed formatting issues

--- src/routes/date.route.js
+++ src/routes/date.route.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const app = express();
+
 
 router.get("/", (res, req) => {
     res.status(200).send("Date page");
-        });
+});
 
 module.exports = router;
--- src/routes/phenomenon.route.js
+++ src/routes/phenomenon.route.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const app = express();
+
 
 router.get("/", (res, req) => {
     res.status(200).send("Phenomenon page");
-        });
+});
 
 module.exports = router;
