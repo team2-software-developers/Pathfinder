e8ae00a1cce0775ab1595796c048a43485a8d67d
RomanHyde <romanhyde1@gmail.com>
2021-07-08 10:26:54 +0100

    #23 fixed export

--- src/routes/phenomenon.route.js
+++ src/routes/phenomenon.route.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 
-router.get("/", (req,res) => {
+router.get("/", (req, res) => {
     res.status(200).send("Phenomenon page");
     // res.status(200).json([
     //     {id:1, username:"someone"},
     //     {id:2, username:"no-one"}
     // ]);
 });
+
+module.exports = router;
