13174b52ef764c38d5543380f055726d5a8a5d40
Minakrv <mnkrv@yahoo.com>
2021-07-08 23:11:22 +0100

    #29 create utils foolder in front end and set up connection with the database and front end

---
+++ client/src/utils/index.js
@@ -0,0 +1,18 @@
+const fetchInfo = async () => {
+    const response = await fetch("http://localhost:5000/get");
+    const data = await response.json();
+    return data;
+};
+
+const fetchDataLocation = async (inputLocation) => {
+    const response = await fetch("http://localhost:5000/get/location", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            
+            location: inputLocation
+        })
+    })
+    const data = await response.json()
+    return data
+};
